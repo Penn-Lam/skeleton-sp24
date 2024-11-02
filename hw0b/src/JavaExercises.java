509d8061d5d513d7082d6d6c7e96947928914448
Penn_Lam <lemonmonlucy@163.com>
2024-11-02 16:28:08 +0800

    finished JavaExercises

--- hw0b/src/JavaExercises.java
+++ hw0b/src/JavaExercises.java
@@ -6,8 +6,9 @@ public class JavaExercises {
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
         // TODO: Fill in this function.
-        return null;
+        return new int[]{1, 2, 3, 4, 5, 6};
     }
+
 
     /** Returns the order depending on the customer.
      *  If the customer is Ergun, return ["beyti", "pizza", "hamburger", "tea"].
@@ -15,14 +16,29 @@ public class JavaExercises {
      *  In any other case, return an empty String[] of size 3. */
     public static String[] takeOrder(String customer) {
         // TODO: Fill in this function.
-        return null;
+        if (customer == "Ergun") {
+            return new String[]{"beyti", "pizza", "hamburger", "tea"};
+        }else if (customer == "Erik") {
+            return new String[]{"sushi", "pasta", "avocado", "coffee"};
+        }
+        return new String[3];
     }
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
         // TODO: Fill in this function.
-        return 0;
+        int max = 0;
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return max - min;
     }
 
     /**
@@ -39,7 +55,17 @@ public class JavaExercises {
 
     private static List<Integer> hailstoneHelper(int x, List<Integer> list) {
         // TODO: Fill in this function.
-        return null;
+        list.add(x);
+        while(x!=1){
+            if(x%2==0){
+                x=x/2;
+                list.add(x);
+            }else{
+                x=x*3+1;
+                list.add(x);
+            }
+        }
+        return list;
     }
 
 }
